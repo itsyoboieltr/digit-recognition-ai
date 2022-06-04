6a042d82712092f1404029e79e5492b1ea5944bf
itsyoboieltr <72046715+itsyoboieltr@users.noreply.github.com>
2022-06-04 18:20:24 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: process.env.VITE_BASE ?? '/',
   plugins: [react()],
+  build: { minify: 'esbuild', target: 'esnext' },
 });
